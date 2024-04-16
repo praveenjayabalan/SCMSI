d46e0fcbf55ca3dcac624cab85bc200b85782474
Praveen <pravin2007.cbe@gmail.com>
2024-04-16 14:13:48 +0530

    aa

--- Webapp/course/models.py
+++ Webapp/course/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class CourseMaster(models.Model):
   course_name = models.CharField(max_length=100)
   course_desc = models.CharField(max_length=255)
+
+  def __str__(self):
+      return self.course_desc+' ('+self.course_name+')'
