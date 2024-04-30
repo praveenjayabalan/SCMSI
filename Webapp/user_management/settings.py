055c87ac26bfe0bfbc9ebddf1b6f3ac9aee30cb3
praveenjayabalan <pravin2007.cbe@gmail.com>
2024-04-30 23:48:19 +0530

    Update settings.py

--- Webapp/user_management/settings.py
+++ Webapp/user_management/settings.py
@@ -178,6 +178,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'pravin2007.cbe@gmail.com'#sender's email-id
-EMAIL_HOST_PASSWORD = 'dxip dsed ueyo znep' #password associated with above email-id
+EMAIL_HOST_USER = 'bmscw021@gmail.com'
+EMAIL_HOST_PASSWORD = 'kqll ieec wfbc xtle' #password associated with above email-id
 
